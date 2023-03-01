8076a92a5f90a8968d94f514fc43bbe388ac01ee
Leo Schneider <leocoelho256@gmail.com>
2023-03-01 16:00:06 -0300

    criando um try catch no controller de register

--- back-end/src/Controllers/registerController.js
+++ back-end/src/Controllers/registerController.js
@@ -2,14 +2,17 @@ const createNewUser = require('../Services/registerService');
 const createToken = require('../utils/auth/createToken');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-  const response = await createNewUser({ name, email, password });
+  try {
+    const { name, email, password } = req.body;
 
-  const { id: ID, role: ROLE, name: NAME } = response.dataValues;
-
-  const token = createToken({ id: ID, role: ROLE, name: NAME });
-
-  return res.status(201).json({ token });
+    const response = await createNewUser({ name, email, password });
+    const { id: ID, role: ROLE, name: NAME } = response.dataValues;
+    const token = createToken({ id: ID, role: ROLE, name: NAME });
+  
+    res.status(201).json({ token, role: ROLE, name: NAME, email });
+  } catch (error) {
+    res.status(409).json({ error });
+  }
 };
 
 module.exports = register;
