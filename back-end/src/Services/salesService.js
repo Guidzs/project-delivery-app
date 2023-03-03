af804160dbdf6fb97aea4f9605fe3ad3909b1825
Leo Schneider <leocoelho256@gmail.com>
2023-03-03 17:16:59 -0300

    alterando o back end do requisito 04. Colocando a inserção nas tabelas sales e salesProducts para rodar, front-end em andamento ainda

--- back-end/src/Services/salesService.js
+++ back-end/src/Services/salesService.js
@@ -2,20 +2,27 @@ const { sales, users, sales_products: SalesProducts,
    products: Products } = require('../database/models');
 
 const newSales = async (body) => {
-  const { seller, products, customer, deliveryAddress, deliveryNumber } = body;
+  const { seller, products, customer, deliveryAddress, deliveryNumber, totalPrice } = body;
+  // Pegar os ids do seller e customer que são entregues pelo app com o nome do usuário
   const { dataValues: { id: sellerId } } = await users.findOne({ where: { name: seller } });
   const { dataValues: { id: userId } } = await users.findOne({ where: { name: customer } });
-  const sale = await sales.create({
-    userId,
-    sellerId,
-    deliveryAddress,
-    deliveryNumber,
- });
-  await Promise.all(products.map(async ({ name, quantity }) => {
+  // Inserir dados na tabela sales
+  const { dataValues: { id: saleId } } = await sales
+    .create({
+      userId,
+      sellerId,
+      deliveryAddress,
+      deliveryNumber,
+      totalPrice,
+    });
+  // Inserir valores dentro da tabela SalesProducts. Pegamos os produtos e dispachamos 1 por 1.
+  await products.map(async ({ name, quantity }) => {
+    // pegando o id de cada produto a partir do nome do produto
     const { dataValues: { id: productId } } = await Products.findOne({ where: { name } });
-    await SalesProducts.create({ productId, saleId: sale.dataValues.id, quantity });
-  }));
-  return sale;
+    // Inserindo dados na SalesProducts
+    await SalesProducts.create({ productId, saleId, quantity });
+  });
+  return saleId;
 };
 
 module.exports = { newSales };
