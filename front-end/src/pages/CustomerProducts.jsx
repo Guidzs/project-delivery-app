bb3d49e3aaa5978e9d730cc958ba26338c403b63
Leo Schneider <leocoelho256@gmail.com>
2023-03-07 01:05:26 -0300

    descobri um problema, quando colocamos muitos produtos o código simplesmente falha!

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import CustomerProduct from '../components/CustomerProduct';
 import NavBar from '../components/NavBar';
 import ShoppingCart from '../components/ShoppingCart';
