07a1d345cebcfaa18c82ab18211a71819309edcf
Leo Schneider <leocoelho256@gmail.com>
2023-03-07 00:56:01 -0300

    resetando carrinho quando voltar pra products

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react';
 import CustomerProduct from '../components/CustomerProduct';
 import NavBar from '../components/NavBar';
 import ShoppingCart from '../components/ShoppingCart';
+import context from '../context/Context';
 import axios from '../utils/connectionDatabase';
 
 export default function CustomerProducts() {
+  const { setCart } = useContext(context);
   const [products, setProducts] = useState([]);
 
   useEffect(
     () => {
+      setCart([]);
       const fetchProducts = async () => {
         const productsResponse = await axios.get('/products');
         setProducts(productsResponse.data);
