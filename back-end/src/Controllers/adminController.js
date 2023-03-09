d170b7c238b08fa6f28c1a43d2dd8b37e6a0322b
Leo Schneider <leocoelho256@gmail.com>
2023-03-09 16:54:13 -0300

    finalizamos o backend do administrador

---
+++ back-end/src/Controllers/adminController.js
@@ -0,0 +1,42 @@
+const {
+  insertUserService,
+  removeUserService,
+  getAllUsersListService,
+} = require('../Services/adminService');
+
+const insertUserController = async (req, res) => { // função de controle de inserção de novo usuário no banco de dados
+  try {
+    const response = await insertUserService(req.body);
+    res.status(201).json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeUserController = async (req, res) => { // função de controle de remoção do cadastro de usuário no banco de dados
+  try {
+    console.log('Amigo Estou aqui 1');
+    const { id } = req.params;
+    const response = await removeUserService(id);
+    console.log(response);
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllUsersListController = async (_req, res) => { // função de controle de exibição da lista de todos os usuários do banco de dados
+  try {
+    console.log('amigo estou aqui!')
+    const response = await getAllUsersListService();
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  insertUserController,
+  removeUserController,
+  getAllUsersListController,
+};
---
+++ back-end/src/routers/adminRoutes.js
@@ -0,0 +1,21 @@
+// importações de bibliotecas
+const express = require('express');
+
+// importações de arquivos do projeto
+const validateNewUser = require('../middleware/validateNewUser');
+const adminVerifyJwt = require('../middleware/adminVerifyJwt');
+const {
+  insertUserController,
+  removeUserController,
+  getAllUsersListController,
+} = require('../Controllers/adminController');
+
+// SOMENTE IMPORTAÇõES DA APLICAÇÃO ACIMA
+
+
+const router = express.Router(); // criação do router
+router.get('/get/userlist', adminVerifyJwt, getAllUsersListController); // declaração de rota get('/admin/get/userlist')
+router.delete('/remove/user/:id', adminVerifyJwt, removeUserController); // declaração da rota delete('/admin/remove/user/:id')
+router.post('/register/newuser', adminVerifyJwt, validateNewUser, insertUserController); // declaração da rota post('/admin/register/newuser')
+
+module.exports = router;
