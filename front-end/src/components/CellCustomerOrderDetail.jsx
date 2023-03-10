78469bc40937f6140bfbb7f1cb17320172506571
Leticia-C <leticia.camposg93@gmail.com>
2023-03-10 19:12:57 -0300

    Adiciona SellerOrder.css

--- front-end/src/components/CellCustomerOrderDetail.jsx
+++ front-end/src/components/CellCustomerOrderDetail.jsx
@@ -8,43 +8,41 @@ export default function CellCustomerOrderDetail(
   const history = useHistory();
 
   return (
-    <div>
-      <button
-        type="button"
-        className="customerOrders"
-        onClick={ () => history.push(`/customer/orders/${UUID}`) }
+    <button
+      type="button"
+      className="customerOrders"
+      onClick={ () => history.push(`/customer/orders/${UUID}`) }
+    >
+
+      <div
+        className="idOrder"
+        data-testid={ `customer_orders__element-order-id-${index + 1}` }
       >
+        { id }
+      </div>
+
+      <div
+        className={ `statusOrder statusOrder-${status}` }
+        data-testid={ `customer_orders__element-delivery-status-${index + 1}` }
+      >
+        { status }
+      </div>
+      <div className="dataEPrice">
+        <div
+          className="dataOrder"
+          data-testid={ `customer_orders__element-order-date-${index + 1}` }
+        >
+          { data }
+        </div>
 
         <div
-          className="idOrder"
-          data-testid={ `customer_orders__element-order-id-${index + 1}` }
+          className="priceOrder"
+          data-testid={ `customer_orders__element-card-price-${index + 1}` }
         >
-          { id }
+          { totalPrice }
         </div>
-
-        <div
-          className="statusOrder"
-          data-testid={ `customer_orders__element-delivery-status-${index + 1}` }
-        >
-          { status }
-        </div>
-        <div className="dataEPrice">
-          <div
-            className="dataOrder"
-            data-testid={ `customer_orders__element-order-date-${index + 1}` }
-          >
-            { data }
-          </div>
-
-          <div
-            className="priceOrder"
-            data-testid={ `customer_orders__element-card-price-${index + 1}` }
-          >
-            { totalPrice }
-          </div>
-        </div>
-      </button>
-    </div>
+      </div>
+    </button>
   );
 }
 
--- front-end/src/pages/SellerOrder.jsx
+++ front-end/src/pages/SellerOrder.jsx
@@ -20,58 +20,55 @@ function SellerOrders() {
   return (
     <div>
       <Navbar />
-      <div>
+      <div className="customer-orders-component ">
         { orders === undefined
           ? <p> Loging... </p>
           : (
             orders.products.map((order) => (
               <button
+                className="customerOrders sellerOrders"
                 key={ order.id }
                 type="button"
                 onClick={ () => history.push(`/seller/orders/${order.id}`) }
-                className="order_card"
               >
                 <p
+                  className="idOrder"
                   data-testid={ `seller_orders__element-order-id-${order.id}` }
-
                 >
-                  {' '}
-                  Pedido Nº
-                  {' '}
+
                   {String(order.id).padStart(orderNumberLength, '0')}
                 </p>
+                <div className="adressOrder">
+                  <div className="status-dataOrder">
+                    <p
+                      className={ `statusOrder statusOrder-${order.status}` }
+                      data-testid={ `seller_orders__element-delivery-status-${order.id}` }
+                    >
+                      {order.status}
+                    </p>
+                    <div className="dataEPrice">
+                      <p
+                        className="dataOrder"
+                        data-testid={ `seller_orders__element-order-date-${order.id}` }
+                      >
+                        { moment(`${order.saleDate}`).format('DD/MM/YYYY') }
+                      </p>
 
-                <p
-                  data-testid={ `seller_orders__element-delivery-status-${order.id}` }
-                >
-                  <b>Status:</b>
-                  {' '}
-                  {order.status}
-                </p>
-
-                <p
-                  data-testid={ `seller_orders__element-order-date-${order.id}` }
-                >
-                  <b>Data:</b>
-                  {' '}
-                  { moment(`${order.saleDate}`).format('DD/MM/YYYY') }
-                </p>
-
-                <p
-                  data-testid={ `seller_orders__element-card-price-${order.id}` }
-                >
-                  <b>Valor Total:</b>
-                  {' '}
-                  {`R$ ${order.totalPrice.toString().replace('.', ',')}` }
-                </p>
-
-                <p
-                  data-testid={ `seller_orders__element-card-address-${order.id}` }
-                >
-                  <b>Endereço de Entrega:</b>
-                  {' '}
-                  { `${order.deliveryAddress}, ${order.deliveryNumber}` }
-                </p>
+                      <p
+                        className="priceOrder"
+                        data-testid={ `seller_orders__element-card-price-${order.id}` }
+                      >
+                        {`R$ ${order.totalPrice.toString().replace('.', ',')}` }
+                      </p>
+                    </div>
+                  </div>
+                  <p
+                    className="streetOrder"
+                    data-testid={ `seller_orders__element-card-address-${order.id}` }
+                  >
+                    { `${order.deliveryAddress}, ${order.deliveryNumber}` }
+                  </p>
+                </div>
               </button>
             ))
           ) }
